b762ecf0e3f8fe42b0e8ebbcb6a9cb34325f8283
nguyendangkhoa2007 <dangkhoa3207@gmail.com>
2025-03-25 19:38:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function login()
+  {
+   let u = username.value
+   let p = pw.value
+   if ( u == "trung" && p == "1909")
+   {
+     window.open("home.html") 
+     document.getElementById(compiler).style.display = "block"
+     }
+}
